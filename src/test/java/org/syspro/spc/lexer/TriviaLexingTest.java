b8cbb88eb38b4526ee609141ab640de601377099
Yury Kareba <wwsh@gmail.com>
2024-10-31 12:47:59 +0700

    Tests are updated

--- src/test/java/org/syspro/spc/lexer/TriviaLexingTest.java
+++ src/test/java/org/syspro/spc/lexer/TriviaLexingTest.java
@@ -1,8 +1,14 @@
 package org.syspro.spc.lexer;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.sypro.spc.lexer.SpcLexer;
+import syspro.tm.lexer.Token;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TriviaLexingTest {
 
@@ -11,7 +17,65 @@ public class TriviaLexingTest {
     public void indentationAndCommentaries() {
         String input = "\n  #*comment*";
         SpcLexer lex = new SpcLexer();
-        lex.lex(input);
+        List<Token> result = (lex.lex(input));
+        List<Token> expected = new ArrayList<>();
+        Assertions.assertEquals( expected, result);
+    }
+
+    @Test
+    @DisplayName("Simple Indent")
+    public void SimpleIndent() {
+        String input = "\n  .";
+        SpcLexer lex = new SpcLexer();
+        List<String> result = (lex.lex(input)).stream().map(Token::toString).toList();
+
+        List<String> expected = Arrays.asList(
+                "<INDENT>", "."
+        );
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Simple Indent with CRLF")
+    public void SimpleIndentCRLF() {
+        String input = "\r\n  .";
+        SpcLexer lex = new SpcLexer();
+        List<String> result = (lex.lex(input)).stream().map(Token::toString).toList();
+
+        List<String> expected = Arrays.asList(
+                "<INDENT>", "."
+        );
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Simple Indent Dedent")
+    public void SimpleIndentDedent() {
+        String input = "\n  .\n";
+        SpcLexer lex = new SpcLexer();
+        List<String> result = (lex.lex(input)).stream().map(Token::toString).toList();
+
+        List<String> expected = Arrays.asList(
+                "<INDENT>", ".", "<DEDENT>"
+        );
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Simple Indent Dedent with CRLF")
+    public void SimpleIndentDedentCRLF() {
+        String input = "\r\n  .\r\n";
+        SpcLexer lex = new SpcLexer();
+        List<String> result = (lex.lex(input)).stream().map(Token::toString).toList();
+
+        List<String> expected = Arrays.asList(
+                "<INDENT>", ".", "<DEDENT>"
+        );
+
+        Assertions.assertEquals(expected, result);
     }
 
     @Test
